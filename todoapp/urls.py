646c88e5f81931126a59b71556bf89955062b869
Mathan495 <mathansk2710@gmail.com>
2026-03-19 18:17:44 +0530

    modify the urlsname

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home ,name='homepage'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('delete-task/<str:name>/', views.Deletetask, name='delete'),
+    path('delete-task/<int:name>/', views.Deletetask, name='delete'),
     path('update-task/<str:name>/', views.Update, name='update'),
     path('logout/', views.logout_view, name='logout'),
 ]
