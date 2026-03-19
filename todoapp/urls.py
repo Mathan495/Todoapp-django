2c6a73b95aec0d62728b2b7ae522fc62f9a07d86
Mathan495 <mathansk2710@gmail.com>
2026-03-19 18:38:47 +0530

    fix id

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home ,name='homepage'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('delete-task/<int:name>/', views.Deletetask, name='delete'),
-    path('update-task/<int:name>/', views.Update, name='update'),
+    path('delete-task/<int:id>/', views.Deletetask, name='delete'),
+    path('update-task/<int:id>/', views.Update, name='update'),
     path('logout/', views.logout_view, name='logout'),
 ]
